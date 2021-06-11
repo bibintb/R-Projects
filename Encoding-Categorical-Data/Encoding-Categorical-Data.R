bb8b0c30276fc4b99bce7c76ec7896d6c7943454
bibin t b <bibintb5@gmail.com>
2021-06-11 21:12:27 +0530

    Create Encoding-Categorical-Data.R

---
+++ Encoding-Categorical-Data/Encoding-Categorical-Data.R
@@ -0,0 +1,20 @@
+#data processing 
+
+#importing datadset
+dataset = read.csv('data.csv')
+
+#taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+dataset$Country = factor(dataset$Country, 
+                         levels = c('France','Spain','Germany'), # c is a vector 
+                         labels = c(1,2,3)) # which number has to given for France,Spain,Germany
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No','Yes'),
+                         labels = c(0,1)) # which number has to given for 'No','Yes'
